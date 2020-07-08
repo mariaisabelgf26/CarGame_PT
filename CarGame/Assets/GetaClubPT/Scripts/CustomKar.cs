d1b6749cf4f318b79fdb9cd94f84c418a79d382c
mariaisabelgf26 <mariaisabelgf26@gmail.com>
2020-07-08 17:19:07 -0500

    Initial Commit. Prueba Tecnica para GetaClub. En lo que se crea features tales como customize karts y correr con ellos

---
+++ CarGame/Assets/GetaClubPT/Scripts/CustomKar.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomKar : MonoBehaviour
+{
+    [Header("Wheel Colors")]
+    public Material[] wheel_Colors;
+    [Header("Driver Colors")]
+    public Material[] driver_Colors;
+    [Header("Kart Colors")]
+    public Material[] kart_Colors;
+
+    private GameObject kart_Display;
+    private GameObject[] objectsToChangeMat;
+    private Material[] materialToChange;
+    private Renderer objMat;
+    private int currentMat = 0;
+
+    void Start()
+    {
+        kart_Display = GameObject.Find("KartClassic_Player");
+        DontDestroyOnLoad(kart_Display);
+    }
+
+    public void changeWheelColor()
+    {
+        materialToChange = wheel_Colors;
+        objectsToChangeMat = new GameObject[4];
+        objectsToChangeMat[0] = kart_Display.transform.Find("KartWheels").transform.Find("WheelFrontLeft").gameObject;
+        objectsToChangeMat[1] = kart_Display.transform.Find("KartWheels").transform.Find("WheelFrontRight").gameObject;
+        objectsToChangeMat[2] = kart_Display.transform.Find("KartWheels").transform.Find("WheelrearLeft").gameObject;
+        objectsToChangeMat[3] = kart_Display.transform.Find("KartWheels").transform.Find("WheelsRearRight").gameObject;
+
+        currentMat = 0;
+    }
+
+    public void changeDriverColor()
+    {
+        materialToChange = driver_Colors;
+        objectsToChangeMat = new GameObject[1];
+        objectsToChangeMat[0] = kart_Display.transform.Find("KartSuspension").transform.Find("PlayerIdle").transform.Find("Template_Character").gameObject;
+
+        currentMat = 0;
+    }
+
+    public void changeKartColor()
+    {
+        materialToChange = kart_Colors;
+        objectsToChangeMat = new GameObject[1];
+        objectsToChangeMat[0] = kart_Display.transform.Find("KartSuspension").transform.Find("Kart").transform.Find("Kart_Body").gameObject;
+
+        currentMat = 0;
+    }
+
+    public void changeLeft()
+    {
+        currentMat = currentMat - 1;
+        int tempInt = Mathf.Abs(currentMat) % materialToChange.Length;
+
+        for (int i = 0; i < objectsToChangeMat.Length; i++ )
+        {
+            objMat = objectsToChangeMat[i].GetComponent<Renderer>();
+            objMat.material = materialToChange[tempInt];
+        }
+    }
+
+    public void changeRight()
+    {
+        currentMat = currentMat + 1;
+        int tempInt = Mathf.Abs(currentMat) % materialToChange.Length;
+
+        for (int i = 0; i < objectsToChangeMat.Length; i++)
+        {
+            objMat = objectsToChangeMat[i].GetComponent<Renderer>();
+            objMat.material = materialToChange[tempInt];
+        }
+    }
+}
---
+++ CarGame/Assets/GetaClubPT/Scripts/SetKartOnGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetKartOnGame : MonoBehaviour
+{
+    private GameObject kart_Player;
+    private Transform kart_Transform;
+
+    private GameObject cinematic_Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kart_Player = GameObject.Find("KartClassic_Player");
+        cinematic_Camera = GameObject.Find("CinemachineVirtualCamera");
+
+        //Reset Camera to follow custom kart
+        cinematic_Camera.GetComponent<Cinemachine.CinemachineVirtualCamera>().Follow = kart_Player.transform;
+        cinematic_Camera.GetComponent<Cinemachine.CinemachineVirtualCamera>().LookAt = kart_Player.transform;
+
+        // Reset Scripts
+
+        kart_Player.GetComponent<Rigidbody>().useGravity = true;
+        kart_Player.GetComponent<KartGame.KartSystems.ArcadeKart>().enabled = true;
+        kart_Player.GetComponent<KartGame.KartSystems.KartPlayerAnimator>().enabled = true;
+        kart_Player.GetComponent<KartGame.KartSystems.KartAnimation>().enabled = true;
+        kart_Player.GetComponent<KartGame.KartSystems.KartBounce>().enabled = true;
+        kart_Player.GetComponent<RotateOnAxis>().enabled = false;
+
+        // Reset Position
+        kart_Transform = kart_Player.GetComponent<Transform>();
+        kart_Transform.position = new Vector3(15.94f, 1.01f, 1.5f);
+        kart_Transform.rotation = Quaternion.Euler(0, 0, 0);
+        kart_Transform.localScale = new Vector3(1, 1, 1);
+        kart_Player.SetActive(false);
+        kart_Player.SetActive(true);
+    }
+}
